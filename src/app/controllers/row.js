11e914c105a846371e6a835f17dcd8831dd8d864
Jerome Musialak <jerome@musialak.fr>
2014-08-07 10:13:58 +0200

    Remove useless function args alertSrv

--- src/app/controllers/row.js
+++ src/app/controllers/row.js
@@ -8,7 +8,7 @@ function (angular, app, _) {
 
   var module = angular.module('kibana.controllers');
 
-  module.controller('RowCtrl', function($scope, $rootScope, $timeout, $routeParams, $modal, $q, ejsResource, querySrv, alertSrv) {
+  module.controller('RowCtrl', function($scope, $rootScope, $timeout, $routeParams, $modal, $q, ejsResource, querySrv) {
       var _d = {
         title: "Row",
         height: "150px",
