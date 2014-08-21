eae0f50450d1df61dd148ff8c88a1caa2d324ec4
Julien Palard <julien@palard.fr>
2014-08-21 17:10:28 +0200

    Force panel: Refresh panel when dashboard reloads.

--- src/app/panels/force/module.js
+++ src/app/panels/force/module.js
@@ -70,6 +70,9 @@ define([
 
     $scope.init = function() {
       console.log('force scope init');
+      $scope.$on('refresh',function(){
+        $scope.get_data();
+      });
       $scope.get_data();
     };
 
